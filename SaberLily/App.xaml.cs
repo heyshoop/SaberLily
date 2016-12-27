ddbcb8051ef720791826d75a0e042ae06c410cc9
阁楼麻雀 <netuser.orz@icloud.com>
2016-12-27 20:46:52 +0800

    完成登录功能 TODO：处理登录后数据

--- SaberLily/App.xaml.cs
+++ SaberLily/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace SaberLily
 {
@@ -13,5 +14,25 @@ namespace SaberLily
     /// </summary>
     public partial class App : Application
     {
+        private static DispatcherOperationCallback exitFrameCallback = new DispatcherOperationCallback(ExitFrame);
+        public static void DoEvents()
+        {
+            DispatcherFrame nestedFrame = new DispatcherFrame();
+            DispatcherOperation exitOperation = Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Background, exitFrameCallback, nestedFrame);
+            Dispatcher.PushFrame(nestedFrame);
+            if (exitOperation.Status !=
+            DispatcherOperationStatus.Completed)
+            {
+                exitOperation.Abort();
+            }
+        }
+
+        private static Object ExitFrame(Object state)
+        {
+            DispatcherFrame frame = state as
+            DispatcherFrame;
+            frame.Continue = false;
+            return null;
+        }
     }
 }
